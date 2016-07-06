48fa0b1c810ca7392f629df5a1ae7f64ab1fbe9c
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-07-06 11:51:09 -0700

    Robot

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class HelloWorld {
+public static void main(String[]x){
+//JOptionPane.showMessageDialog(null, "Hello, it's me");
+	Robot jeff=new Robot();
+	Robot steve=new Robot(100,100);
+	jeff.penDown();
+	jeff.setPenWidth(30);
+	jeff.setPenColor(Color.BLUE);
+	int i=1;
+	while(i<5){
+	
+
+		
+		}
+
+	jeff.move(250);
+	jeff.turn(90);
+	i+=1;
+	}
+	}	
+}
---
+++ src/PentCray.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+/* Teacher’s note: before beginning, draw a pentagon and have students work out the angle that the robot will have to turn (360/5) */
+
+public class PentCray {
+
+	private void makePrettyThings() {
+		// 1. Create a new Robot
+		Robot jeff=new Robot();
+		// 3. Put the robot's pen down
+		jeff.penDown();
+		
+		// 8. Make the robot go at maximum speed (10)
+		jeff.setSpeed(10);
+		// 9. Set the pen to a color that you like for the shape
+		
+		// 4. Make a variable for the number of sides you want (can’t test this one)
+		int side=10;
+		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
+		int var=360/5;
+		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
+		for(int i=1;i<201;i++) {
+			jeff.setRandomPenColor();
+			// 2. Move the robot 200 pixels
+		jeff.move(i);
+			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
+			// 6. Turn the robot the amount in your angle variable
+		jeff.turn(var);
+			// 11. Turn the robot one more degree
+		jeff.turn(1);
+		}
+	}
+
+	// Variations:
+	// *12. make the pattern really huge
+	// *13. randomize the color of the pattern 
+	// *14. experiment with different shapes
+
+	public static void main(String[] args) {
+		new PentCray().makePrettyThings();
+	}
+}
